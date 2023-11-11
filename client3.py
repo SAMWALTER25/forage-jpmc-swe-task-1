9dcb13df98c8effd097565d7b7f9bf689e9225bf
sam_walter <samwaltermathari@gmail.com>
2023-11-11 17:11:25 +0530

    pass

--- client3.py
+++ client3.py
@@ -35,6 +35,8 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
+
+    """----Did changes here for Task 1 JPMC ----"""
     price = (bid_price + ask_price)/2
     return stock, bid_price, ask_price, price
 
@@ -43,7 +45,7 @@ def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
     if (price_b == 0):
-        return
+        return            """"Task 1 changes  lines 47,48,49"""
     return (price_a / price_b)
 
 
